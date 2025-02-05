274db484f1d4a357fc525ba421aeb18f2a3db426
lui7z <luiz727@gmail.com>
2025-02-05 20:29:23 -0300

    Att

--- scripts/script.js
+++ scripts/script.js
@@ -5,20 +5,15 @@ function carregarFuncoes(){
     ordemPrioridade();
 }
 
-var prioridade = document.getElementsByClassName("prioridade");
-
 function corPrioridade() {
-    var prioridade = document.getElementsByClassName("prioridade");
-    Array.from(prioridade).forEach(prio => {
+    var prioridades = document.getElementsByClassName("prioridade");
+    Array.from(prioridades).forEach(prio => {
         if (prio.textContent == 'Alta') {
-            prio.style.backgroundColor = '#CF3F3F';
-            prio.style.border = '0px solid #CF3F3F';
+            prio.classList.add('prioridade-alta');
         } else if (prio.textContent == 'Média') {
-            prio.style.backgroundColor = '#FFBD3D';
-            prio.style.border = '0px solid #FFBD3D';
+            prio.classList.add('prioridade-media');
         } else if (prio.textContent == 'Baixa') {
-            prio.style.backgroundColor = '#72C240';
-            prio.style.border = '0px solid #72C240';
+            prio.classList.add('prioridade-baixa');
         }
     });
 }
@@ -28,8 +23,9 @@ function ordemPrioridade(){
     var altaPrioridade = [];
     var mediaPrioridade = [];
     var baixaPrioridade = [];
+    var prioridades = document.getElementsByClassName("prioridade");
 
-    Array.from(prioridade).forEach(prio => {
+    Array.from(prioridades).forEach(prio => {
         var lista = prio.closest("ul");
         if (prio.textContent == 'Alta') {
             altaPrioridade.push(lista);
